005f7394796052f96a5a0341cbc8d985e539abfa
Sumrit <sumritarora.512@gmail.com>
2023-03-10 13:41:14 +0530

    first commit

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -5,29 +5,33 @@ import BundleAnalyzer from '@next/bundle-analyzer'
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
 
 const withBundleAnalyzer = BundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
+  enabled: 
+  // process.env.ANALYZE === 
+  'true',
 })
 
 // const withTM = NextTranspileModules([])
 
 const withVanillaExtract = createVanillaExtractPlugin()
 
-// const sentryWebpackPluginOptions =
-//   process.env.VERCEL_ENV === 'production'
-//     ? {
-//         // Additional config options for the Sentry Webpack plugin. Keep in mind that
-//         // the following options are set automatically, and overriding them is not
-//         // recommended:
-//         //   release, url, org, project, authToken, configFile, stripPrefix,
-//         //   urlPrefix, include, ignore
-//         silent: false, // Logging when deploying to check if there is any problem
-//         validate: true,
-//         // https://github.com/getsentry/sentry-webpack-plugin#options.
-//       }
-//     : {
-//         silent: true, // Suppresses all logs
-//         dryRun: !process.env.SENTRY_AUTH_TOKEN,
-//       }
+const sentryWebpackPluginOptions =
+  // process.env.VERCEL_ENV === 
+  // 'production'
+  //   ?
+     {
+        // Additional config options for the Sentry Webpack plugin. Keep in mind that
+        // the following options are set automatically, and overriding them is not
+        // recommended:
+        //   release, url, org, project, authToken, configFile, stripPrefix,
+        //   urlPrefix, include, ignore
+        silent: false, // Logging when deploying to check if there is any problem
+        validate: true,
+        // https://github.com/getsentry/sentry-webpack-plugin#options.
+      }
+    // : {
+    //     silent: true, // Suppresses all logs
+    //     dryRun: !process.env.SENTRY_AUTH_TOKEN,
+    //   }
 
 /** @type {import('next').NextConfig} */
 const config = {
@@ -174,4 +178,4 @@ const config = {
   },
 }
 
-export default withBundleAnalyzer(withVanillaExtract(withSentryConfig(withAxiom(config))))
+export default withBundleAnalyzer(withVanillaExtract(withSentryConfig(withAxiom(config), sentryWebpackPluginOptions)))
