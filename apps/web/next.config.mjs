b697388f849245c32cfec07d0e15e2caf7e3b02d
Sumrit <sumritarora.512@gmail.com>
2023-03-10 13:19:27 +0530

    first commit

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -12,22 +12,22 @@ const withBundleAnalyzer = BundleAnalyzer({
 
 const withVanillaExtract = createVanillaExtractPlugin()
 
-const sentryWebpackPluginOptions =
-  process.env.VERCEL_ENV === 'production'
-    ? {
-        // Additional config options for the Sentry Webpack plugin. Keep in mind that
-        // the following options are set automatically, and overriding them is not
-        // recommended:
-        //   release, url, org, project, authToken, configFile, stripPrefix,
-        //   urlPrefix, include, ignore
-        silent: false, // Logging when deploying to check if there is any problem
-        validate: true,
-        // https://github.com/getsentry/sentry-webpack-plugin#options.
-      }
-    : {
-        silent: true, // Suppresses all logs
-        dryRun: !process.env.SENTRY_AUTH_TOKEN,
-      }
+// const sentryWebpackPluginOptions =
+//   process.env.VERCEL_ENV === 'production'
+//     ? {
+//         // Additional config options for the Sentry Webpack plugin. Keep in mind that
+//         // the following options are set automatically, and overriding them is not
+//         // recommended:
+//         //   release, url, org, project, authToken, configFile, stripPrefix,
+//         //   urlPrefix, include, ignore
+//         silent: false, // Logging when deploying to check if there is any problem
+//         validate: true,
+//         // https://github.com/getsentry/sentry-webpack-plugin#options.
+//       }
+//     : {
+//         silent: true, // Suppresses all logs
+//         dryRun: !process.env.SENTRY_AUTH_TOKEN,
+//       }
 
 /** @type {import('next').NextConfig} */
 const config = {
@@ -162,16 +162,16 @@ const config = {
       },
     ]
   },
-  webpack: (webpackConfig, { webpack }) => {
-    // tree shake sentry tracing
-    webpackConfig.plugins.push(
-      new webpack.DefinePlugin({
-        __SENTRY_DEBUG__: false,
-        __SENTRY_TRACING__: false,
-      }),
-    )
-    return webpackConfig
-  },
+  // webpack: (webpackConfig, { webpack }) => {
+  //   // tree shake sentry tracing
+  //   webpackConfig.plugins.push(
+  //     new webpack.DefinePlugin({
+  //       __SENTRY_DEBUG__: false,
+  //       __SENTRY_TRACING__: false,
+  //     }),
+  //   )
+  //   return webpackConfig
+  // },
 }
 
-export default withBundleAnalyzer(withVanillaExtract(withSentryConfig(withAxiom(config), sentryWebpackPluginOptions)))
+export default withBundleAnalyzer(withVanillaExtract(withSentryConfig(withAxiom(config))))
